2ef7fbbf45fa32be18123224f431debd1fdc0931
Noah <noah.hellman@protonmail.com>
2018-03-21 22:16:56 +0100

    clean up and add options to ghdl compile script

--- aims.sh
+++ aims.sh
@@ -1,23 +1,64 @@
-# Usage: ./aims ENTITY SIMULATION_TIME
+#!/bin/sh
 
-set -e
+USAGE="Usage: ./aims [OPTIONS]...
 
-alu_src="tests/z80/alu_tb.vhd src/z80/alu.vhd"
+Options:
+    -h                      show this help
+    -f SRC_FILES_FILE       src files in build/srclists/SRC_FILE_FILE, all 
+    -m ENTITY               make executable from src files
+                            .vhd files in below tree will be used if -s 
+                            not specified
+    -s                      run simulation with created executable
+    -t TIME                 simulation time
+    -a                      stop simulation on assertion error
+    -c                      clean up build files and executable
+"
 
-src=$alu_src
-entity=$1
-simtime=$2
+# actions
+quit=false          # quit immediately
+make=false          # make executable
+sim=false           # run simulation
+clean=false    # remove executable
 
-# compile
-ghdl -a $src        # analyze designs
-ghdl -i $src        # import designs
-ghdl -m $entity     # make executable
+src=$(find . -name '*.vhd')
+entity=
+args=
 
-# simulate
-./$entity --wave=wave.ghw  --stop-time=$simtime
+while getopts hm:f:st:awcC OPT; do
+    case $OPT in
+        h) quit=true ;;
+        f) src=$(cat build/srclists/$OPTARG) ;;
+        m) entity=$OPTARG; make=true ;;
+        s) sim=true ;;
+        t) args="$args --stop-time=$OPTARG" ;;
+        a) args="$args --assert-level=error" ;;
+        c) clean=true ;;
+        [?]) quit=true ;;
+    esac
+done
 
-# show wave file
-#gtkwave wave.ghw
+echo src: $src
+echo args: $args
+echo entity: $entity
 
-# clean up
-rm -f wave.ghw *.o $1
+if [ "$quit" = true ]; then
+    echo "$USAGE"
+    exit 1
+fi
+
+if [ "$make" = true ]; then
+    ghdl -a $src        # analyze designs
+    ghdl -i $src        # import designs
+    ghdl -m $entity     # make executable
+fi
+
+if [ "$sim" = true ]; then
+    ./$entity $args --wave=wave.ghw
+fi
+
+if [ "$clean" = true ]; then
+    rm -f $entity 
+    rm -f wave.ghw
+fi
+
+rm -f *.o
