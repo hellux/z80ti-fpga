2a6da10508c8b9157cd13a8f2620b0f79d3dcc05
hellux <hellux@protonmail.com>
2018-03-23 21:58:56 +0100

    add, rename options

--- aims.sh
+++ aims.sh
@@ -5,14 +5,23 @@ USAGE="Usage: ./aims [OPTIONS]...
 Options:
     -h                      show this help
     -A                      analyze src files
-    -M ENTITY               analyze, make ENTITY
-    -S ENTITY               analyze, make, simulate ENTITY
+    -M ENTITY               analyze, make executable for ENTITY
+    -S ENTITY               analyze, make, simulate ENTITY and generate wave
     -C                      clean up build files and executable
-    -f SRC_FILES_FILE       src files in build/srclists/SRC_FILE_FILE, all 
-                            .vhd files in below tree will be used if -s 
-                            not specified
+    -u SRC_LIST             unit, name of file with list of src files in
+                            build/srclists, all .vhd files in below tree will
+                            be used if -f not specified
     -a                      abort simulation on assertion error
     -t TIME                 simulation time
+    -w FILENAME             name of generated wave file -- default: wave.ghw
+
+Examples:
+    simulate alu_tb when all alu src files listed in build/srclists/alu
+        ./aims -u alu -S alu_tb
+    analyze all alu src files
+        ./aims -u alu -A
+    analyze all files in src/z80
+        ./aims -f \"src/z80/*.vhd\" -A
 "
 
 # actions
@@ -25,17 +34,20 @@ clean=false         # remove executable
 src=$(find . -name '*.vhd')
 entity=""
 args=""
+wave="wave.ghw"
 
-while getopts hAM:S:Cf:at: OPT; do
+while getopts hAM:S:Cf:e:at:w: OPT; do
     case $OPT in
         h) quit=true ;;
         A) analyze=true ;;
         M) analyze=true; make=true;          entity=$OPTARG ;;
         S) analyze=true; make=true; sim=true entity=$OPTARG ;;
         C) clean=true ;;
-        f) src=$(cat build/srclists/$OPTARG) ;;
+        f) src=$OPTARG ;;
+        u) src=$(cat build/srclists/$OPTARG) ;;
         a) args="$args --assert-level=error" ;;
         t) args="$args --stop-time=$OPTARG" ;;
+        w) wave=$OPTARG ;;
         [?]) quit=true ;;
     esac
 done
@@ -52,7 +64,7 @@ if [ "$analyze" = true ]; then
         ghdl -m $entity # make executable
 
         if [ "$sim" = true ]; then
-            ./$entity $args --wave=wave.ghw
+            ./$entity $args --wave=$wave
         fi
     fi
 fi
