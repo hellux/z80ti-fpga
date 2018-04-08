017dec954a9ee71b5ddfbf917119545768b50f7b
hellux <hellux@protonmail.com>
2018-04-08 08:35:29 +0200

    mv legacy files to separate folder, removed unused code

--- src/z80/registers.vhd
+++ src/z80/registers.vhd
@@ -1,5 +1,5 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity reg_8 is port(
     clk, rst : in std_logic;
@@ -24,8 +24,8 @@ begin
     do <= bits when wr = '1' else (others => 'Z');
 end Behavioral;
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity reg_16 is port( 
     clk, rst : in std_logic;
@@ -51,46 +51,9 @@ begin
     do <= bits when wr = '1' else (others => 'Z');
 end Behavioral;
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
-entity reg_pair is port(
-    clk, rst : in std_logic;
-    rd, wr, swp : in std_logic;
-    di : in std_logic_vector(7 downto 0);
-    do : out std_logic_vector(7 downto 0));
-end reg_pair;
-
-architecture Behavioral of reg_pair is
-    component reg_8 port (
-        clk, rst : in std_logic;
-        rd, wr : in std_logic;
-        di : in std_logic_vector(7 downto 0);
-        do : out std_logic_vector(7 downto 0));
-    end component;
-    
-    signal di_x_prim : std_logic_vector(7 downto 0);
-    signal do_x_prim : std_logic_vector(7 downto 0);
-    signal di_x      : std_logic_vector(7 downto 0);
-    signal do_x      : std_logic_vector(7 downto 0);
-    signal rd_x      : std_logic;
-    signal wr_x      : std_logic;
-begin  
-    X : reg_8 port map(clk, rst, rd_x, wr_x, di_x, do_x);
-    X_prim : reg_8 port map(clk, rst, rd=>swp, wr=>swp,
-                            di=>di_x_prim, do=>do_x_prim);
-
-    rd_x <= rd or swp;
-    wr_x <= wr or swp;
-    di_x <= do_x_prim   when swp = '1' else 
-            di          when swp = '0';
-    do <= do_x when wr = '1' else "ZZZZZZZZ";
-    di_x_prim <= do_x;
-end Behavioral;
-
 -- buffers, like registers but write on same cp
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity buf8 is port(
     clk, rst : in std_logic;
@@ -115,8 +78,8 @@ begin
     do <= bits_next when wr = '1' else (others => 'Z');
 end arch;
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity buf16 is port(
     clk, rst : in std_logic;
