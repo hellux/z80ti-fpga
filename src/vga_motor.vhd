f7773582bc1976f00b6615cf67f915760cfd2223
Noah <noah.hellman@protonmail.com>
2018-04-18 08:16:53 +0200

    restore vga motor

--- src/vga_motor.vhd
+++ src/vga_motor.vhd
@@ -1,13 +1,14 @@
-library ieee;
-use ieee.std_logic_1164.all;            -- basic ieee library
-use ieee.numeric_std.all;               -- ieee library for the unsigned type
+-- library declaration
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;            -- basic IEEE library
+use IEEE.NUMERIC_STD.ALL;               -- IEEE library for the unsigned type
 
 
 -- entity
 entity vga_motor is port ( 
      clk			: in std_logic;
-	 data			: in std_logic_vector(7 downto 0);
-	 addr			: out std_logic_vector(9 downto 0);
+	 data			: in std_logic;
+	 addr			: out std_logic_vector(12 downto 0);
 	 rst			: in std_logic;
 	 vgaRed		    : out std_logic_vector(2 downto 0);
 	 vgaGreen	    : out std_logic_vector(2 downto 0);
@@ -30,8 +31,6 @@ architecture Behavioral of vga_motor is
   signal    blank       : std_logic;                        -- blanking signal
 
 	
-    signal current_pixel : std_logic;
-    signal pixel_index : integer range 0 to 6143;
 begin
 
   -- Clock divisor
@@ -49,8 +48,10 @@ begin
 	
   -- 25 MHz clock (one system clock pulse width)
   vga_clk <= '1' when (clk_count = 3) else '0';
-
+	
+	
   -- Horizontal pixel counter
+
   process(clk)
   begin
     if rising_edge(clk) then
@@ -65,7 +66,8 @@ begin
         end if;
     end if;
   end process;
-
+            
+  
   -- Horizontal sync
 
   Hsync <= '0' when Xpixel > 656 and Xpixel <= 752 else
@@ -99,16 +101,15 @@ begin
            '0';
   
   -- Tile memory
-  -- varför klockad process? blir en puls fördröjning
   process (clk) begin
     if rising_edge(clk) then
       if (blank = '0') then
         if Xpixel > 576 or Ypixel > 384 then
             colour <= "00000000";
         else
-            if (current_pixel = '1') then
+            if (data = '1') then
                 colour <= "11111111";
-            elsif (current_pixel = '0') then
+            elsif (data = '0') then
                 colour <= "01001000";
             end if;
         end if;
@@ -119,21 +120,27 @@ begin
   end process;
   
   -- Picture memory address composite (16 = 96//6, 10 = 64//2)
-    current_pixel <= data(pixel_index rem 8);
-    pixel_index <= to_integer(xpixel/6) + to_integer(ypixel*24);
-    addr <= std_logic_vector(to_unsigned(pixel_index/8, addr'length));
+  addr <= std_logic_vector(resize(Xpixel/4 + Ypixel*24, addr'length));
   -- Set picture colour based on pos and data. (576 = 96*6, 384 = 64*6)
 
   -- Text: (10010110) Backgroud: (01001000) Bar: (00000000)
   -- VGA generation
     vgaRed(2) 	<= colour(7);
+                   
     vgaRed(1) 	<= colour(6);
+                   
     vgaRed(0) 	<= colour(5);
+                   
     vgaGreen(2) <= colour(4);
+                   
     vgaGreen(1) <= colour(3);
+                   
     vgaGreen(0) <= colour(2);
+                   
     vgaBlue(2) 	<= colour(1);
+                   
     vgaBlue(1) 	<= colour(0);
+
 
 end Behavioral;
 
