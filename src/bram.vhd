059ab4467bfb5d87a9bc225453c9234d480c4573
Noah <noah.hellman@protonmail.com>
2018-04-20 09:26:36 +0200

    mv bram to separate entity

---
+++ src/bram.vhd
@@ -0,0 +1,36 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity bram is generic(
+    dwidth : integer;
+    size : integer;
+    awidth : integer); port(
+    clk, rst : in std_logic;
+    wea, web : in std_logic;
+    addra, addrb : in std_logic_vector(awidth-1 downto 0);
+    data_ina, data_inb : in std_logic_vector(dwidth-1 downto 0);
+    data_outa, data_outb : out std_logic_vector(dwidth-1 downto 0));
+end bram;
+
+architecture arch of bram is
+    type bram_mem_t is array(0 to size-1) of
+        std_logic_vector(dwidth-1 downto 0); 
+    signal mem : bram_mem_t;
+begin
+    process(clk) begin
+        if rising_edge(clk) then
+            if wea='1' then
+                mem(to_integer(unsigned(addra))) <= data_ina;
+            end if;
+            if web='1' then
+                mem(to_integer(unsigned(addrb))) <= data_inb;
+            end if;
+            if rst = '1' then
+                mem <= (others => (others => '0'));
+            end if;
+            data_outa <= mem(to_integer(unsigned(addra)));
+            data_outb <= mem(to_integer(unsigned(addrb)));
+        end if;
+    end process;
+end arch;
--- src/pict_mem.vhd
+++ src/pict_mem.vhd
@@ -2,6 +2,8 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+-- TODO do_lcd
+
 entity pict_mem is port (
     clk, clk_z80, clk_vga, rst : in std_logic;
     rd, wl : in std_logic;
@@ -14,27 +16,48 @@ entity pict_mem is port (
     do_lcd: out std_logic_vector(7 downto 0));
 end pict_mem;
 
-architecture Behavioral of pict_mem is
-    type gmem_state_t is (idle, load);
-    type gmem_mem_t is array(0 to 7679) of std_logic;
+architecture arch of pict_mem is
+    component bram generic(
+        dwidth : integer;
+        size : integer;
+        awidth : integer); port(
+        clk, rst : in std_logic;
+        wea, web : in std_logic;
+        addra, addrb : in std_logic_vector(awidth-1 downto 0);
+        data_ina, data_inb : in std_logic_vector(dwidth-1 downto 0);
+        data_outa, data_outb : out std_logic_vector(dwidth-1 downto 0));
+    end component;
 
-    signal pic_mem : gmem_mem_t;
-    signal a_lcd, a_vga : integer range pic_mem'range;
+    type gmem_state_t is (idle, load);
+
+    signal gmem_we_lcd : std_logic;
+    signal gmem_di_lcd : std_logic_vector(0 downto 0);
+    signal gmem_do_lcd, gmem_do_vga : std_logic_vector(0 downto 0);
+    signal gmem_a_lcd, gmem_a_vga : std_logic_vector(12 downto 0);
     signal page_buf : std_logic_vector(7 downto 0);
     signal bit_out, bit_in : integer range 0 to 7;
     signal state : gmem_state_t;
 begin
-    -- helpers
-    process(x_lcd, y_lcd, wl)
+    gmem : bram generic map(1, 7679, 13) port map(clk, rst,
+                                                 gmem_we_lcd, '0',
+                                                 gmem_a_lcd, gmem_a_vga,
+                                                 gmem_di_lcd, "0", 
+                                                 gmem_do_lcd, gmem_do_vga);
+
+    process(x_lcd, y_lcd, wl, bit_in)
         variable xl : integer range 0 to 63;
         variable yl : integer range 0 to 19;
     begin
         xl := to_integer(unsigned(x_lcd));
         yl := to_integer(unsigned(y_lcd));
         if wl = '1' then
-            a_lcd <= xl*120+yl*8;
+            gmem_a_lcd <= std_logic_vector(to_unsigned(
+                          xl*120+yl*8 + bit_in,
+                          13));
         else
-            a_lcd <= xl*120+yl*6;
+            gmem_a_lcd <= std_logic_vector(to_unsigned(
+                          xl*120+yl*6 + bit_in,
+                          13));
         end if;
     end process;
 
@@ -44,10 +67,14 @@ begin
     begin
         xv := to_integer(unsigned(x_vga));
         yv := to_integer(unsigned(y_vga));
-        a_vga <= yv*120+xv;
+        gmem_a_vga <= std_logic_vector(to_unsigned(yv*120+xv, 13));
     end process;
 
-    bram_in_lcd : process(clk) begin
+    gmem_we_lcd <= '1' when state = load else '0';
+    gmem_di_lcd(0) <= page_buf(7-bit_in);
+    do_vga <= gmem_do_vga(0);
+
+    lcd : process(clk) begin
         if rising_edge(clk) then
             if clk_z80 = '1' then
                 if rd = '1' then
@@ -56,8 +83,13 @@ begin
                 end if;
             end if;
 
+            if bit_out = 7 then
+                bit_out <= 0;
+            else
+                bit_out <= bit_out + 1;
+            end if;
+
             if state = load then
-                pic_mem(a_lcd+bit_in) <= page_buf(7-bit_in);
                 if (wl = '1' and bit_in = 7) or (wl = '0' and bit_in = 5) then
                     bit_in <= 0;
                     state <= idle;
@@ -65,32 +97,6 @@ begin
                     bit_in <= bit_in + 1;
                 end if;
             end if;
-
-            if rst = '1' then
-                pic_mem <= (others => '0');
-            end if;
         end if;
     end process;
-
-    bram_out_lcd : process(clk) begin
-        if rising_edge(clk) then
-            do_lcd(7-bit_out) <= pic_mem(a_lcd+bit_out);
-            if wl = '1' and bit_out = 7 then
-                bit_out <= 0;
-            elsif wl = '0' and bit_out = 5 then
-                bit_out <= 0;
-            else
-                bit_out <= bit_out + 1;
-            end if;
-        end if;
-    end process;
-
-    bram_out_vga : process(clk) begin
-        if rising_edge(clk) then
-            if clk_vga = '1' then
-                do_vga <= pic_mem(a_vga);
-            end if;
-        end if;
-    end process;
-end Behavioral;
-
+end arch;
