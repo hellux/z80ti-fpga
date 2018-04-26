c51e14ad38bfd0b576685956cc2fc4ed55f42424
Dennis Derecichei <dende301@mux1-08.edu.isy.liu.se>
2018-04-26 12:33:11 +0200

    add states when E0

--- src/io/kbd_enc.vhd
+++ src/io/kbd_enc.vhd
@@ -27,13 +27,13 @@ architecture behavioral of KBD_ENC is
   signal make_q			      : std_logic;			              -- make one pulselse flip flop
   signal make_op		      : std_logic;			              -- make one pulse
 
-  type state_type is (idle, make, break);			              -- declare state types for ps2
+  type state_type is (idle, make, break,make_ext,break_ext );			              -- declare state types for ps2
   signal ps2_state : state_type;					              -- ps2 state
 
-  signal scancode		      : std_logic_vector(15 downto 0); 	  -- scan code
-  signal keycode              : std_logic_vector(7 downto 0);     -- key code
+  signal scancode		      : std_logic_vector(7 downto 0); 	  -- scan code
+  signal keycode, keycode_ext : std_logic_vector(7 downto 0);     -- key code
   signal keys_down_int        : keys_down_t := (others => x"ff"); -- keys_down mtrx set to 1 in evert group
-  signal on_key_down          : std_logic;                        -- On key
+  signal on_key_down_int      : std_logic;                        -- On key
   
   type wr_type is (standby, wrchar, wrcur);			              -- declare state types for write cycle
   signal wrstate : wr_type;					                      -- write cycle state
@@ -106,63 +106,90 @@ begin
   -- *                                 *
   -- ***********************************
       process(clk)
-        variable grp : integer range 0 to 6;
-        variable key : integer range 0 to 7;
-      begin
+        variable grp, grp_ext : integer range 0 to 6;
+        variable key, key_ext : integer range 0 to 7;
+    begin
         grp := to_integer(unsigned(keycode(7 downto 4)));
         key := to_integer(unsigned(keycode(3 downto 0)));
+        grp_ext := to_integer(unsigned(keycode_ext(7 downto 4)));
+        key_ext := to_integer(unsigned(keycode_ext(3 downto 0)));
         if rising_edge(clk) then
-          if rst='1' then
-            ps2_state <= idle;
-          elsif ps2_state = idle then 
-            if ps2bitcounter = 11 and scancode /= X"F0" then
-                ps2_state <= make;
-                keys_down_int(grp)(key) <= '0'; -- declare key pressed down
-            elsif ps2bitcounter = 11 and scancode = X"F0" then 
-                ps2_state <= break;
-            end if;
-          elsif ps2_state = make then 
-                ps2_state <= idle;
-          elsif ps2_state = break then
-            if ps2bitcounter = 11 then 
-                keys_down_int(grp)(key) <= '1';  
+            if rst='1' then
                 ps2_state <= idle;
             end if;
-          end if;
+            case ps2_state is
+            when idle =>
+                if ps2bitcounter = 11 then
+                    case scancode is
+                    when x"F0" =>
+                        ps2_state <= break;            
+                    when x"E0" => 
+                        ps2_state <= make_ext;
+                    when others =>
+                        ps2_state <= make;
+                        keys_down_int(grp)(key) <= '0'; -- declare key pressed down
+                    end case;
+                end if;
+            when make =>
+                ps2_state <= idle;
+            when make_ext =>
+                if ps2bitcounter = 11 then
+                    keys_down_int(grp_ext)(key_ext) <= '0'; -- released key
+                    ps2_state <= idle;
+                end if;
+            when break =>
+                if ps2bitcounter = 11 then
+                    case scancode is
+                    when x"e0" =>
+                        ps2_state <= break_ext;
+                    when others =>
+                        keys_down_int(grp)(key) <= '1'; -- released key 
+                        ps2_state <= idle;
+                    end case;
+                end if;
+            when break_ext =>
+                if ps2bitcounter = 11 then
+                    keys_down_int(grp_ext)(key_ext) <= '1'; -- released key 
+                    ps2_state <= idle;
+                end if;
+            end case;
         end if;
-     end process;
+    end process;
 	
 
   -- Scan Code -> Key Code mapping
   with scancode select
-   keycode <=    x"00" when x"E072",	-- KEY DOWN  | KP_Down
-                 x"01" when x"E06B",	-- KEY LEFT  | KP_Left
-                 x"02" when x"E074",	-- KEY RIGHT | KP_Right
-                 x"03" when x"E075",	-- KEY UP    | KP_Up
-                 x"10" when x"005A",	-- ENTER     | ENTER
-                 x"11" when x"0079",	-- ADD +     | KP_Add 
-                 x"12" when x"007B",	-- SUB -     | KP_Sub
-                 x"13" when x"007C",	-- MULT x    | 
-                 x"14" when x"004E",	-- DIV \     | (+ ? \)   
-                 x"15" when x"005B",	-- POWER ^   | (Key next to Å)
-                 x"16" when x"0077",	-- CLEAR     | Num Lock
-                 x"41" when x"0016",	-- 1         | 1
-                 x"31" when x"001E",	-- 2         | 2
-                 x"21" when x"0026",	-- 3         | 3
-                 x"42" when x"0025",	-- 4         | 4
-                 x"32" when x"002E",	-- 5         | 5
-                 x"22" when x"0036",	-- 6         | 6
-                 x"43" when x"003D",	-- 7         | 7
-                 x"33" when x"003E",	-- 8         | 8
-                 x"23" when x"0046",	-- 9         | 9
-                 x"40" when x"0045",	-- 0         | 0
-		         x"65" when x"000D",	-- 2ND       | TAB
-                 x"00FF" when others;
-  -- we will be enabled ('1') for two consecutive clock 
-  -- cycles during WRCHAR and WRCUR states
-  -- and disabled ('0') otherwise at STANDBY state
-  we <= '0' when (wrstate = standby) else 
-        '1';
+   keycode <=    x"00" when x"72",	-- KEY DOWN  | KP_Down
+                 x"01" when x"6B",	-- KEY LEFT  | KP_Left
+                 x"02" when x"74",	-- KEY RIGHT | KP_Right
+                 x"03" when x"75",	-- KEY UP    | KP_Up
+                 x"10" when x"5A",	-- ENTER     | ENTER
+                 x"11" when x"79",	-- ADD +     | KP_Add 
+                 x"12" when x"7B",	-- SUB -     | KP_Sub
+                 x"13" when x"7C",	-- MULT x    | 
+                 x"14" when x"4E",	-- DIV \     | (+ ? \)   
+                 x"15" when x"5B",	-- POWER ^   | (Key next to Å)
+                 x"16" when x"77",	-- CLEAR     | Num Lock
+                 x"41" when x"16",	-- 1         | 1
+                 x"31" when x"1E",	-- 2         | 2
+                 x"21" when x"26",	-- 3         | 3
+                 x"42" when x"25",	-- 4         | 4
+                 x"32" when x"2E",	-- 5         | 5
+                 x"22" when x"36",	-- 6         | 6
+                 x"43" when x"3D",	-- 7         | 7
+                 x"33" when x"3E",	-- 8         | 8
+                 x"23" when x"46",	-- 9         | 9
+                 x"40" when x"45",	-- 0         | 0
+		         x"65" when x"0D",	-- 2ND       | TAB
+                 x"FF" when others;
+
+    with scancode select keycode_ext <= 
+        x"00" when x"72",	-- KEY DOWN  | KP_Down
+        x"01" when x"6B",	-- KEY LEFT  | KP_Left
+        x"02" when x"74",	-- KEY RIGHT | KP_Right
+        x"03" when x"75",	-- KEY UP    | KP_Up
+        x"F0" when x"5A",	-- ENTER     | ENTER
+        x"FF" when others;
   
   -- set as keycode
   data <=  keycode;
