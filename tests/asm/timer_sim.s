0de941c8d248cf04ee04db7ccdd20a62e6f5abc8
Noah <noah.hellman@protonmail.com>
2018-04-27 17:46:42 +0200

    add timer sim test

---
+++ tests/asm/timer_sim.s
@@ -0,0 +1,19 @@
+;Setup up a timer that waits 2 seconds
+   di
+   ld a,0
+   out ($35),a ; disable/reset timer 2
+   out ($38),a ; disable/reset timer 2
+   ld a,$84      ; cpu freq
+   out ($30),a
+   ld a,0        ; no loop, no interrupt
+   out ($31),a
+   ld a,2       ; set timer to 16
+   out ($32),a
+wait:
+   in a,(4)
+   bit 5,a       ;bit 5 tells if timer 1 is done
+   jr z,wait
+   xor a
+   out ($30),a   ;Turn off the timer.
+   out ($31),a
+   halt
