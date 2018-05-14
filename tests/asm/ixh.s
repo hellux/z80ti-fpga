307edde21c1c2134d8f4ea531c77d39f3bcc07a3
hellux <hellux@protonmail.com>
2018-05-14 23:21:01 +0200

    fix sll instr (set bit 0)

---
+++ tests/asm/ixh.s
@@ -0,0 +1,19 @@
+ld a, 0xfe
+ld ix, 0xabcd
+; and ixh
+db 0xdd
+db 0xa4
+jp p, fail
+jp z, fail
+jp po, fail
+jp c, fail
+cp 0xaa
+jp nz, fail
+
+success:
+ld a, 0xcc
+halt
+
+fail:
+ld a, 0xee
+halt
