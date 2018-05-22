fad9ce4ea69a19c342cd850d8b74b13f309982cc
hellux <hellux@protonmail.com>
2018-05-22 23:15:30 +0200

    refix lcd 8-bit

--- src/ti/pict_mem.vhd
+++ src/ti/pict_mem.vhd
@@ -35,11 +35,11 @@ architecture arch of pict_mem is
     signal gmem_di_lcd : std_logic_vector(0 downto 0);
     signal gmem_do_lcd, gmem_do_vga : std_logic_vector(0 downto 0);
     signal gmem_a_lcd, gmem_a_vga : std_logic_vector(12 downto 0);
-    signal page_buf : std_logic_vector(7 downto 0);
     signal x_buf : std_logic_vector(5 downto 0);
     signal y_buf : std_logic_vector(4 downto 0);
-    signal bit_sel : integer range 0 to 7;
+    signal bit_sel, page_bit : integer range 0 to 7; -- bit in mem byte, page
     signal state : gmem_state_t;
+    signal word_length : integer range 6 to 8;
 begin
     gmem : bram generic map(1, LCD_COLS*LCD_ROWS, 13)
         port map(clk, 
@@ -48,58 +48,53 @@ begin
                  gmem_di_lcd, "0", 
                  gmem_do_lcd, gmem_do_vga);
 
-    process(x_buf, y_buf, wl, bit_sel)
-        variable xl : integer range 0 to LCD_ROWS-1;
-        variable yl : integer range 0 to LCD_COLS/6-1;
-        variable wordl : integer range 6 to 8;
+    lcd_addr : process(x_buf, y_buf, wl, bit_sel)
+        variable xl, yl : integer;
     begin
         xl := to_integer(unsigned(x_buf));
         yl := to_integer(unsigned(y_buf));
-        if wl = '1' then wordl := 8; else wordl := 6; end if;
         gmem_a_lcd <= std_logic_vector(to_unsigned(
-            xl*LCD_COLS+yl*wordl + bit_sel, 13
+            xl*LCD_COLS+yl*word_length + bit_sel, 13
         ));
     end process;
 
-    process(x_vga, y_vga)
-        variable xv : integer range 0 to 128;
-        variable yv : integer range 0 to 64;
+    vga_addr : process(x_vga, y_vga)
+        variable xv, yv : integer;
     begin
         xv := to_integer(unsigned(x_vga));
         yv := to_integer(unsigned(y_vga));
         gmem_a_vga <= std_logic_vector(to_unsigned(yv*LCD_COLS+xv, 13));
     end process;
 
+    --> gmem
+    word_length <= 8 when wl = '1' else 6;
+    page_bit <= word_length-1-bit_sel when word_length-1-bit_sel >= 0 else 0;
     gmem_we_lcd <= '1' when state = load else '0';
-    gmem_di_lcd(0) <= page_buf(7-bit_sel) when wl = '1' else
-                      page_buf(5-bit_sel) when bit_sel < 6 else
-                      '-';
+    gmem_di_lcd(0) <= page_in(page_bit);
+
+    --> vga
     do_vga <= gmem_do_vga(0);
+
+    --> lcd
+    do_lcd(bit_sel) <= gmem_do_lcd(0);
 
     lcd : process(clk) begin
         if rising_edge(clk) then
-            if bit_sel = 7 then
-                bit_sel <= 0;
-            else
-                bit_sel <= bit_sel + 1;
+            if bit_sel >= word_length-1
+            then bit_sel <= 0;
+            else bit_sel <= bit_sel + 1;
             end if;
 
             case state is
             when load =>
-                if (wl = '1' and bit_sel = 7) or
-                   (wl = '0' and bit_sel = 5)
-                then
-                    bit_sel <= 0;
+                if bit_sel >= word_length-1 then
                     state <= idle;
                 end if;
             when idle =>
-                do_lcd(bit_sel) <= gmem_do_lcd(0);
-
+                x_buf <= x_lcd;
+                y_buf <= y_lcd;
                 if rd = '1' then
                     state <= load;
-                    page_buf <= page_in;
-                    x_buf <= x_lcd;
-                    y_buf <= y_lcd;
                     bit_sel <= 0;
                 end if;
             end case;
