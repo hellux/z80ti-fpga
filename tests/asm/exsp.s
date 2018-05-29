55711d3898bba0a7e2a8be028821d6926c8ce1af
hellux <hellux@protonmail.com>
2018-05-29 20:15:15 +0200

    add assertions to ex (sp), hl test

--- tests/asm/exsp.s
+++ tests/asm/exsp.s
@@ -1,11 +1,39 @@
-ld sp, 0x20
-ld bc, 0x1234
-ld (0x20), bc
-ld hl, 0x5678
-ex (sp), hl
-ld sp, 0x30
-ld bc, 0x4321
-ld (0x30), bc
-ld iy, 0x8765
-ex (sp), iy
-halt
+org 0x9d95
+
+    ld sp, 0xffff
+    ld bc, 0x1234
+    push bc
+    ld hl, 0x5678
+    ex (sp), hl
+    ld a, h
+    cp 0x12
+    jp nz, fail
+    ld a, l
+    cp 0x34
+    jp nz, fail
+    pop hl
+    ld a, h
+    cp 0x56
+    jp nz, fail
+    ld a, l
+    cp 0x78
+    jp nz, fail
+
+    ld sp, 0xfffe
+    ex (sp), hl
+    ld hl, 0x0000
+    add hl, sp
+    ld a, h
+    cp 0xff
+    jp nz, fail
+    ld a, l
+    cp 0xfe
+    jp nz, fail
+    
+
+success:
+    ld a, 0xcc
+    halt
+fail:
+    ld a, 0xee
+    halt
